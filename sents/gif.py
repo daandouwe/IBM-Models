0abfd285c1d9f54f952b01397c64f712380e0584
Daan van Stigt <daan@Daans-MacBook-Pro.local>
2017-05-05 15:26:06 +0200

    Edited gif

--- sents/gif.py
+++ sents/gif.py
@@ -1,10 +1,10 @@
 import imageio
 images = []
 
-filenames = ['sents-{}.png'.format(i) for i in range(5)]
-# filenames = ['loaded/IBM2/sents-{}.png'.format(i) for i in range(11)]
+# filenames = ['sents-{}.png'.format(i) for i in range(5)]
+filenames = ['loaded/IBM2/uniform/sents-{}.png'.format(i) for i in range(11)]
 
 for filename in filenames:
     images.append(imageio.imread(filename))
-imageio.mimsave('sents0-5.gif', images, duration=0.7)
-# imageio.mimsave('loaded/IBM2/sents-movie.gif', images, duration=0.7)
+# imageio.mimsave('sents0-4.gif', images, duration=0.7)
+imageio.mimsave('loaded/IBM2/uniform/sents-movie.gif', images, duration=[1.4] + 9*[0.7] + [1.4])
